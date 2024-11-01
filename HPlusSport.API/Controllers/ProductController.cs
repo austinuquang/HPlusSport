bdc58915b227f879106564b38c89138def54ddc3
Austin Uquang <austinuquang@gmail.com>
2024-11-01 09:25:40 +0100

    Add project files.

---
+++ HPlusSport.API/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProuducts()
+        {
+            return "OK";
+        }
+    }
+}
