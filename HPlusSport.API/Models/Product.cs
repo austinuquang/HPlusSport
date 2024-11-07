cf19af387de98767e1dfffbf4cb9703306170a68
Austin Uquang <austinuquang@gmail.com>
2024-11-07 11:09:34 +0100

    master

--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace HPlusSport.API.Models
@@ -6,17 +6,15 @@ namespace HPlusSport.API.Models
     public class Product
     {
         public int Id { get; set; }
-        public string  Sku { get; set; }= string.Empty;
-        public string Name { get; set; }=string.Empty;
+        public string Sku { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-
         public bool IsAvailable { get; set; }
+
+        [Required]
         public int CategoryId { get; set; }
-
         [JsonIgnore]
-
-        public virtual Category Category { get; set; }
-
+        public virtual Category? Category { get; set; }
     }
 }
